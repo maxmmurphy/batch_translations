cee478ccc4f2714d6255ddfac24f35e4c7d66dcc
Max Murphy <mmurphy@elctech.com>
2011-09-01 11:10:41 -0700

    fixing to work with rails 3.0.10 usage is the same

--- lib/batch_translation.rb
+++ lib/batch_translation.rb
@@ -4,11 +4,13 @@ module ActionView
       def globalize_fields_for(locale, *args, &proc)
         raise ArgumentError, "Missing block" unless block_given?
         @index = @index ? @index + 1 : 1
-        object_name = "#{@object_name}[translations_attributes][#{@index}]"
-        object = @object.translations.find_by_locale locale.to_s
-        @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
-        @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
-        @template.fields_for(object_name, object, *args, &proc)
+        object_name = "[translations_attributes][#{@index}]"
+        object = @object.translations.find_by_locale locale.to_s 
+        @template.concat @template.hidden_field_tag("#{@object.class.name.downcase}[#{object_name}][locale]", locale)
+        @template.concat @template.hidden_field_tag("#{@object.class.name.downcase}[#{object_name}][id]", object.id) if !object.nil?
+        tt = fields_for(object_name, object, *args, &proc)
+        @template.concat tt
+        @template
       end
     end
   end
